61f8721462a1e270c17c46264ba53c77dc549530
cassolli <dgcassolli@gmail.com>
2025-01-30 19:48:08 -0300

    Update plugin.go

--- plugin.go
+++ plugin.go
@@ -51,16 +51,10 @@ func New(_ context.Context, next http.Handler, config *Config, name string) (htt
 }
 
 func (a *RemoteAddrPlugin) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	//ip, port, _ := strings.Cut(req.RemoteAddr, ":")
-        parts := strings.Split(reqRemoteAddr, ":")
+        parts := strings.Split(req.RemoteAddr, ":")
 
-        if len(parts) == 2 {
-                ip := parts[0]
-                port := parts[1]
-                fmt.Printf("IP: %s, Port: %s\n", ip, port)
-        } else {
-                fmt.Println("Formato inválido")
-        }
+        ip := parts[0]
+        port := parts[1]
 
 	if a.headers.IP != "" {
 		req.Header.Set(a.headers.IP, ip)
